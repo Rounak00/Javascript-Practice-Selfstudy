a75ea8df24d284bea1ab411c6c1f7b43871f64e6
Rounak00 <80785919+Rounak00@users.noreply.github.com>
2021-06-17 20:56:39 +0530

    Add new file

---
+++ 02.1string-template.js
@@ -0,0 +1,17 @@
+
+// Template strings or Templates literals
+
+// es5
+   console.log("Hello" + "Rounak");
+
+// es6
+    console.log(`HEllo Rounak`);
+
+// es5
+    var name="RM";
+    var sname="MM";
+    console.log("HELLO " + name  + sname);
+// es6
+     var name="RM";
+     var sname="MM";
+     console.log(`Hello ${name} ${sname}`);
