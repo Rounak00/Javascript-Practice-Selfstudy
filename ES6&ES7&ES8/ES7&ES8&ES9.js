6c04f690e4f8b4a7f504abfc36ec5e94add84c07
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2021-10-05 17:36:49 +0530

    commit

--- ES6&ES7&ES8/ES7&ES8&ES9.js
+++ ES6&ES7&ES8/ES7&ES8&ES9.js
@@ -1,3 +1,19 @@
+
+//ES7
+//includes:
+let arr=[1,2,3,4,5];
+console.log(arr.includes(8));
+//its check element wheather it is there or not and return true false
+
+//Exponential:(**)
+console.log(2**2); //2 to the power 2
+
+
+
+
+
+
+//ES8
 //1. String-Prototype Padstart/padend(total length,textToAdd)
 let a=" a";
 console.log(a.padStart(10,"hi"));// first index theke suru hobe 10 ghor cholbe
@@ -45,3 +61,49 @@ console.log(entr);
 let a7=[1,2,3, ,];
 console.log(a7);
 console.log(a7.length);
+
+
+//ES9
+//1. RegEx changes
+
+
+
+
+//2.Rest and spread operator
+
+var maxval = Math.max(1,2,3,4,5,9);
+console.log(maxval);
+
+//* Rest Parameters in ES6 
+//* ...this is called rest parameter
+function sum(...inputs){
+    console.log(...inputs);
+    sumis=0;
+    for(var i of inputs)
+        sumis+=i;
+    console.log(sumis);
+}
+sum(1,2,3,4,5,6);
+
+//spread
+let arr3=[1,4,7,2];
+function sum(x,y,z,w){
+    return x+y+w+z;
+}
+let arr2=sum(...arr3);
+console.log(arr2);
+
+// we can also use spread in array
+var a=[1,2,3,4];
+console.log(...a);
+
+
+//Same as rest but use for array
+let arr=[1 , 2 , 3];
+function sam(a,b,c){
+    console.log(a+b+c);
+}
+sam(...arr);
+
+
+//3.Asynchronous iteration
