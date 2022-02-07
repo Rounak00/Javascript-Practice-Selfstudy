a3f51502344f913642271d587d7387c574e3cc42
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-02-07 01:58:39 +0000

    commit

---
+++ Advance JS Concepts/1.How_it_work.js
@@ -0,0 +1,7 @@
+JS_code-> JS_Engine
+
+In JS_Engine:-
+     JS_code-> Parser (check line by line for error) -> AST(then it produce a Data structure called abstract syntax tree) -> MC (convert in machine code) -> Code Runs
+     
+     
+     
