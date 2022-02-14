9e3acc42f1aa6e2a0080fa7a6932b3c01c5bb776
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-02-14 05:27:38 +0000

    Resolve

---
+++ Advance JS Concepts/7.1Promises.js
@@ -0,0 +1,53 @@
+3Stage of Promises: A promise is an object that may produce a single value some time in the future
+
+  1.Pending
+  2.Resolve
+  3.Reject
+
+//2Syntax are there promise obj and promise constructor
+
+promise constructor;-
+ 
+ const p= new Promise ( (resolve, reject)=>{  //Promise produce part
+       setTimeout(()=>{
+           let roll=[1,2,3,4,5];
+           resolve(roll);
+           reject("your msg")
+       },2000)
+ } ); //here it is executer function
+
+ p.then((roll)=>{ //promise Consume
+     console.log(roll);
+ }).catch((c)=>{console.log(c)});
+
+ #in produce part we will fetch the api and if it t is resolve then we send the value to then part or consume part
+ # if its reject tjen in same way reject go to catch portion
+
+  we can also return promise same as that 
+  Example: 
+     function getbio(id){   
+      return new Promise((resolve, reject)=>{
+        setTimeout((id)=>{
+          let bio={
+              name: "Rounak",
+              age: 12
+          }  
+          resolve("Done")
+          reject("note done");
+        },2000,id)
+      })
+     }
+
+     p.then((roll)=>{ 
+        console.log(roll);
+        getbio(id).then((i)=>{console.log(i)})
+    }).catch((c)=>{console.log(c)});
+
+
+    //we can also write consume portion like this
+    
+    p.then((roll)=>{ 
+        console.log(roll);
+        return getbio(id);
+    }).then((i)=>{console.log(i)})
+    .catch((c)=>{console.log(c)});
