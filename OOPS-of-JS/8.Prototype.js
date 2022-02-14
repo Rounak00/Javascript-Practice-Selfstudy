f09fe9e198e637c28a4c6ed109a2f70dcaaccfbe
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-02-14 04:59:19 +0000

    Update 8.Prototype.js

--- OOPS-of-JS/8.Prototype.js
+++ OOPS-of-JS/8.Prototype.js
@@ -4,3 +4,7 @@
 
 // now assume we have object1 and object2
  object1.__proto__=object2; // and we can access all object2 methods and properties in object1 but its not good way 
+
+
+ //Proto typal Inheritance
+ 
