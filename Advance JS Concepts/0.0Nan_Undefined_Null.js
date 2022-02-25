c3e91206a2c75a01aacde389087b548dfbe98081
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-02-25 20:34:16 +0000

    xommit

---
+++ Advance JS Concepts/0.0Nan_Undefined_Null.js
@@ -0,0 +1,12 @@
+/*
+undefined - undefined
+nan - number
+null - obj
+*/ 
+
+
+undefined: A variable that has not been assigned a value is of type undefined
+
+Nan: In JavaScript, NaN stands for Not a Number. It represents a value which is not a valid number.
+
+NULL: 'Null' is a keyword in JavaScript that signifies 'no value' or nonexistence of any value.
