903d3f1f48c089337dcfa6563786a5f914e1a29f
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-09-24 19:59:09 +0530

    Update 5.1Static_Methods.js

--- OOPS-of-JS/5.1Static_Methods.js
+++ OOPS-of-JS/5.1Static_Methods.js
@@ -9,3 +9,24 @@ car.price();
 //Static methods are soemthin for that we dont need a object. we mainly used it for utility functions
 
 // normal variables and functions in class are known as instance variable and instance methods
+
+
+//static variable
+class Person { 
+    static age = 0; 
+     getAge() { return Person.age++; }
+} 
+const p=new Person();
+console.log(Person.age); 
+console.log(p.getAge());
+console.log(p.getAge());
+
+//Static Method
+class Person { 
+    static age = 0; 
+     static getAge() { return this.age++; }
+} 
+
+console.log(Person.age); 
+console.log(Person.getAge());
+console.log(Person.getAge());
