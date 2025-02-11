b7148817a3145f185f0e8935423444cc02770dde
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2025-02-11 12:22:54 +0530

    Update Call_Apply_Bind.js

--- Pollyfills/Call_Apply_Bind.js
+++ Pollyfills/Call_Apply_Bind.js
@@ -1,1 +1,25 @@
+//Pollyfill for CALL
 
+Function.prototype.MyCall(context={},...args){
+  if(typeOf this !== "function"){ throw("Error, Its not a Function")}
+  context.fn-this;
+  context.fn(...args)
+}
+
+
+//Pollyfill for Apply
+Function.prototype.MyApply(context={},args){
+  if(typeOf this !== "function"){ throw("Error, Its not a Function")}
+  if(Array.isArray(args) !== true){ throw("Error, Its not a Array")}
+  context.fn-this;
+  context.fn(...args)
+}
+
+//Pollyfill for Bind
+Function.prototype.MyBind(context={},...args){
+  if(typeOf this !== "function"){ throw("Error, Its not a Function")}
+  context.fn-this;
+  return function(...newargs){
+      return context.fn(...args,...newargs);
+  }
+}
